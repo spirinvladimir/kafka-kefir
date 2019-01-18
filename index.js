c88532347f45a67a4b8bc4d1d8999953266c5171
Spirin Vladimir <spirin.vladimir@gmail.com>
2019-01-18 10:56:21 +0200

    Update index.js

--- index.js
+++ index.js
@@ -4,13 +4,13 @@ module.exports = topic =>
     Kefir.stream(emitter => {
         var client = new Kafka.KafkaClient({kafkaHost})
         client.on('error', () => emitter.end())
-        client.on('ready', () => client.createTopics([topic], (error, result) => error ? emitter.end() : () =>
-                new Kafka.Offset(client).fetch([{ topic, time: -1}], (err, offsets_per_partitions) => {
-                    var offset = offsets_per_partitions[topic]['0'][0] - 1
-                    var consumer = new Kafka.Consumer(client, [{topic, offset}], {fromOffset: true, groupId: String(Date.now()) + Math.random()})
-                    consumer.on('error',  () => emitter.end())
-                    consumer.on('message', message => emitter.emit(message.value))
-                })
-            )
+        client.on('ready', () => 
+            new Kafka.Offset(client).fetch([{ topic, time: -1}], (err, offsets_per_partitions) => {
+                var offset = offsets_per_partitions[topic]['0'][0] - 1
+                var consumer = new Kafka.Consumer(client, [{topic, offset}], {fromOffset: true, groupId: String(Date.now()) + Math.random()})
+                consumer.on('error',  () => emitter.end())
+                consumer.on('message', message => emitter.emit(message.value))
+            })
         )
     })
+
